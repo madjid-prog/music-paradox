4049eb05fd49d62726309e104c8dfcd516f1c2d5
elskuyy <93878999+elskuyy@users.noreply.github.com>
2022-01-24 11:35:51 +0800

    Update Assistant.py

--- Yukki/Plugins/Assistant.py
+++ Yukki/Plugins/Assistant.py
@@ -16,35 +16,35 @@ __HELP__ = f"""
 
 
 /checkassistant
-- Check the alloted assistant of your chat
+- Periksa asisten yang ditentukan untuk obrolan Anda
 
 
 **Note:**
-- Only for Sudo Users
+- Hanya untuk Pengguna Sudo
 
 {ASSISTANT_PREFIX[0]}block [ Reply to a User Message] 
-- Blocks the User from Assistant Account.
+- Memblokir Pengguna dari Akun Asisten.
 
 {ASSISTANT_PREFIX[0]}unblock [ Reply to a User Message] 
-- Unblocks the User from Assistant Account.
+- Buka blokir Pengguna dari Akun Asisten.
 
 {ASSISTANT_PREFIX[0]}approve [ Reply to a User Message] 
-- Approves the User for DM.
+- Menyetujui Pengguna untuk DM.
 
 {ASSISTANT_PREFIX[0]}disapprove [ Reply to a User Message] 
-- Disapproves the User for DM.
+- Menolak Pengguna untuk DM.
 
 {ASSISTANT_PREFIX[0]}pfp [ Reply to a Photo] 
-- Changes Assistant account PFP.
+- Mengubah PFP akun Asisten.
 
 {ASSISTANT_PREFIX[0]}bio [Bio text] 
-- Changes Bio of Assistant Account.
+- Mengubah Bio Akun Asisten.
 
 /changeassistant [ASS NUMBER]
-- Change the previoius alloted assistant to new one.
+- Ubah asisten yang dialokasikan sebelumnya menjadi yang baru.
 
 /setassistant [ASS NUMBER or Random]
-- Set a assistant account for chat. 
+- Setel akun asisten untuk mengobrol. 
 """
 
 
@@ -63,7 +63,7 @@ async def assis_change(_, message: Message):
     _assistant = await get_assistant(message.chat.id, "assistant")
     if not _assistant:
         return await message.reply_text(
-            "No Pre-Saved Assistant Found.\n\nYou can set Assistant Via /setassistant"
+            "Asisten Pra-Tersimpan Tidak Ditemukan.\n\nAnda dapat mengatur Asisten Via /setassistant"
         )
     else:
         ass = _assistant["saveassistant"]
@@ -72,7 +72,7 @@ async def assis_change(_, message: Message):
     }
     await save_assistant(message.chat.id, "assistant", assis)
     await message.reply_text(
-        f"**Changed Assistant**\n\nChanged Assistant Account from **{ass}** to Assistant Number **{ass_num}**"
+        f"**Mengubah Asisten**\n\nMengubah Akun Asisten dari **{ass}** ke Nomor Asisten **{ass_num}**"
     )
 
 
@@ -94,7 +94,7 @@ async def assis_change(_, message: Message):
     _assistant = await get_assistant(message.chat.id, "assistant")
     if not _assistant:
         await message.reply_text(
-            f"**__Yukki Music Bot Assistant Alloted__**\n\nAssistant No. **{ran_ass}**"
+            f"**__Venz Asisten Bot Musik Diberikan__**\n\nAsisten No. **{ran_ass}**"
         )
         assis = {
             "saveassistant": ran_ass,
@@ -103,7 +103,7 @@ async def assis_change(_, message: Message):
     else:
         ass = _assistant["saveassistant"]
         return await message.reply_text(
-            f"Pre-Saved Assistant Number {ass} Found.\n\nYou can change Assistant Via /changeassistant"
+            f"Nomor Asisten Tersimpan Sebelumnya {ass} Found.\n\nAnda dapat mengubah Asisten Via /changeassistant"
         )
 
 
@@ -112,10 +112,10 @@ async def check_ass(_, message: Message):
     _assistant = await get_assistant(message.chat.id, "assistant")
     if not _assistant:
         return await message.reply_text(
-            "No Pre-Saved Assistant Found.\n\nYou can set Assistant Via /play"
+            "Asisten Pra-Tersimpan Tidak Ditemukan.\n\nAnda dapat mengatur Asisten Via /play"
         )
     else:
         ass = _assistant["saveassistant"]
         return await message.reply_text(
-            f"Pre-Saved Assistant Found\n\nAssistanty Number {ass} "
+            f"Asisten yang Disimpan Sebelumnya Ditemukan\n\nNomor Asisten {ass} "
         )
