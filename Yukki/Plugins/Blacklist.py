7b7a972f69eb67fcfbe8328a87a439f440e5e1c0
elskuyy <93878999+elskuyy@users.noreply.github.com>
2022-01-24 11:58:48 +0800

    Update Blacklist.py

--- Yukki/Plugins/Blacklist.py
+++ Yukki/Plugins/Blacklist.py
@@ -9,19 +9,19 @@ __HELP__ = """
 
 
 /blacklistedchat 
-- Check Blacklisted Chats of Bot.
+- Periksa Obrolan Daftar Hitam Bot.
 
 
-**Note:**
-Only for Sudo Users.
+**Catatan:**
+Hanya untuk Pengguna Sudo.
 
 
 /blacklistchat [CHAT_ID] 
-- Blacklist any chat from using Music Bot
+- Daftar hitam obrolan apa pun dari menggunakan Bot Musik
 
 
 /whitelistchat [CHAT_ID] 
-- Whitelist any blacklisted chat from using Music Bot
+- Daftar putih obrolan apa pun yang masuk daftar hitam dari menggunakan Bot Musik
 
 """
 
@@ -30,7 +30,7 @@ Only for Sudo Users.
 async def blacklist_chat_func(_, message: Message):
     if len(message.command) != 2:
         return await message.reply_text(
-            "**Usage:**\n/blacklistchat [CHAT_ID]"
+            "**Penggunaan:**\n/blacklistchat [CHAT_ID]"
         )
     chat_id = int(message.text.strip().split()[1])
     if chat_id in await blacklisted_chats():
@@ -38,31 +38,31 @@ async def blacklist_chat_func(_, message: Message):
     blacklisted = await blacklist_chat(chat_id)
     if blacklisted:
         return await message.reply_text(
-            "Chat has been successfully blacklisted"
+            "Obrolan telah berhasil masuk daftar hitam"
         )
-    await message.reply_text("Something wrong happened, check logs.")
+    await message.reply_text("Terjadi kesalahan, periksa log.")
 
 
 @app.on_message(filters.command("whitelistchat") & filters.user(SUDOERS))
 async def whitelist_chat_func(_, message: Message):
     if len(message.command) != 2:
         return await message.reply_text(
-            "**Usage:**\n/whitelistchat [CHAT_ID]"
+            "**Penggunaan:**\n/whitelistchat [CHAT_ID]"
         )
     chat_id = int(message.text.strip().split()[1])
     if chat_id not in await blacklisted_chats():
-        return await message.reply_text("Chat is already whitelisted.")
+        return await message.reply_text("Obrolan sudah masuk daftar putih.")
     whitelisted = await whitelist_chat(chat_id)
     if whitelisted:
         return await message.reply_text(
-            "Chat has been successfully whitelisted"
+            "Obrolan telah berhasil masuk daftar putih"
         )
-    await message.reply_text("Something wrong happened, check logs.")
+    await message.reply_text("Terjadi kesalahan, periksa log.")
 
 
 @app.on_message(filters.command("blacklistedchat"))
 async def blacklisted_chats_func(_, message: Message):
-    text = "**Blacklisted Chats:**\n\n"
+    text = "**Obrolan Daftar Hitam:**\n\n"
     j = 0
     for count, chat_id in enumerate(await blacklisted_chats(), 1):
         try:
@@ -72,6 +72,6 @@ async def blacklisted_chats_func(_, message: Message):
         j = 1
         text += f"**{count}. {title}** [`{chat_id}`]\n"
     if j == 0:
-        await message.reply_text("No Blacklisted Chats")
+        await message.reply_text("Tidak Ada Obrolan Daftar Hitam")
     else:
         await message.reply_text(text)
